f07279b32c47e2825f879e2f38fdba89508d1064
Daniel Douglas Soyka <ddsoyka@pm.me>
2020-09-17 11:31:08 -0400

    Updates

---
+++ precache-manifest.07be70fdb72640e987acb6783b133e60.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5aa2dc33440e46bf62569598404c13d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "188e827ed304b1a52ac3",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "1c2bb844bac3523e2267",
+    "url": "/static/css/main.edea807e.chunk.css"
+  },
+  {
+    "revision": "188e827ed304b1a52ac3",
+    "url": "/static/js/2.2ba2e2fe.chunk.js"
+  },
+  {
+    "revision": "88cbcb55f867c7e879dbc5e47becbf3b",
+    "url": "/static/js/2.2ba2e2fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c2bb844bac3523e2267",
+    "url": "/static/js/main.b51e2e20.chunk.js"
+  },
+  {
+    "revision": "0eb85cd2d5255c5189d7",
+    "url": "/static/js/runtime-main.c1d1a942.js"
+  },
+  {
+    "revision": "756707b7e7409d124ef1c447b440a672",
+    "url": "/static/media/canada.756707b7.svg"
+  },
+  {
+    "revision": "c1e769605e4686003967900cb433dc65",
+    "url": "/static/media/facebook.c1e76960.svg"
+  },
+  {
+    "revision": "4d1452520216122f71da13f227bb5a61",
+    "url": "/static/media/github.4d145252.svg"
+  },
+  {
+    "revision": "46b613d71cda66c2bfff70ade62c0a10",
+    "url": "/static/media/group.46b613d7.svg"
+  },
+  {
+    "revision": "7dcd41a7cd7a5620582c38f034268248",
+    "url": "/static/media/help.7dcd41a7.svg"
+  },
+  {
+    "revision": "02a699aa6d01351029cd504da7e2c4a0",
+    "url": "/static/media/language.02a699aa.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "48bda8f3e7cd432b70b74d19b76a6204",
+    "url": "/static/media/mail.48bda8f3.svg"
+  },
+  {
+    "revision": "bd502c37a83b170f711606f17e19e254",
+    "url": "/static/media/password.bd502c37.svg"
+  },
+  {
+    "revision": "96e297b07bdd177a8f0a53b83091ca63",
+    "url": "/static/media/random.96e297b0.svg"
+  },
+  {
+    "revision": "c9f1200836515fd4bacd29160be78732",
+    "url": "/static/media/twitter.c9f12008.svg"
+  },
+  {
+    "revision": "b6e5300da71a840bff0a769e2373c390",
+    "url": "/static/media/uk.b6e5300d.svg"
+  },
+  {
+    "revision": "34777fdc3f6cd3a5a089bf8f9156f3fe",
+    "url": "/static/media/unknown.34777fdc.svg"
+  },
+  {
+    "revision": "38f88714d1f6b481d01651439dfed026",
+    "url": "/static/media/usa.38f88714.svg"
+  },
+  {
+    "revision": "48b61bd8093960dfe2fead075ca6df50",
+    "url": "/static/media/word.48b61bd8.svg"
+  }
+]);
