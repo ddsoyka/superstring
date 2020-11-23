2f77c6c2b29c5f53013cc984d65e480f04c523e8
Daniel Douglas Soyka <ddsoyka@pm.me>
2020-11-23 17:52:22 -0500

    Use Calculator Functions & LaTeX

--- src/feature/convert/ConvertElectrical.tsx
+++ src/feature/convert/ConvertElectrical.tsx
@@ -9,13 +9,15 @@ import {
     Nav
 }
 from 'react-bootstrap';
-import Wrapper from '../../component/Wrapper';
 import MathJax from 'react-mathjax';
+import Wrapper from '../../component/Wrapper';
+import * as Calculator from '../../api/calculator';
 
 const MAXIMUM_RESISTANCE = 1000000.0;
 const MAXIMUM_CURRENT = 1000000.0;
 const MAXIMUM_VOLTAGE = 1000000.0;
 const MAXIMUM_POWER = 1000000.0;
+const CALCULATE_FAIL_MESSAGE = 'Unable to calculate missing values';
 
 const ConvertElectrical: React.FC = () => {
     const [key, setKey] = React.useState('calculator');
@@ -27,28 +29,64 @@ const ConvertElectrical: React.FC = () => {
 
     const calculate = () => {
         if (resistance !== 0.0 && voltage !== 0.0) {
-            setCurrent(voltage / resistance);
-            setPower(voltage * current);
+            const i = Calculator.calculateCurrent(undefined, voltage, resistance);
+            const p = Calculator.calculatePower(undefined, voltage, resistance);
+
+            if (i && p) {
+                setCurrent(i);
+                setPower(p);
+            }
+            else throw Error(CALCULATE_FAIL_MESSAGE);
         }
         else if (resistance !== 0.0 && current !== 0.0) {
-            setVoltage(current * resistance);
-            setPower(voltage * current);
+            const v = Calculator.calculateVoltage(undefined, current, resistance);
+            const p = Calculator.calculatePower(current, undefined, resistance);
+
+            if (v && p) {
+                setVoltage(v);
+                setPower(p);
+            }
+            else throw Error(CALCULATE_FAIL_MESSAGE);
         }
         else if (resistance !== 0.0 && power !== 0.0) {
-            setVoltage(Math.sqrt(power * resistance));
-            setCurrent(power / voltage);
+            const v = Calculator.calculateVoltage(power, undefined, resistance);
+            const i = Calculator.calculateCurrent(power, undefined, resistance);
+
+            if (v && i) {
+                setVoltage(v);
+                setCurrent(i);
+            }
+            else throw Error(CALCULATE_FAIL_MESSAGE);
         }
         else if (current !== 0.0 && voltage !== 0.0) {
-            setResistance(voltage / current);
-            setPower(voltage * current);
+            const r = Calculator.calculateResistance(undefined, voltage, current);
+            const p = Calculator.calculatePower(current, voltage, undefined);
+
+            if (r && p) {
+                setResistance(r);
+                setPower(p);
+            }
+            else throw Error(CALCULATE_FAIL_MESSAGE);
         }
         else if (current !== 0.0 && power !== 0.0) {
-            setVoltage(power / current);
-            setResistance(voltage / current);
+            const v = Calculator.calculateVoltage(power, current, undefined);
+            const r = Calculator.calculateResistance(power, undefined, current);
+
+            if (v && r) {
+                setVoltage(v);
+                setResistance(r);
+            }
+            else throw Error(CALCULATE_FAIL_MESSAGE);
         }
         else if (voltage !== 0.0 && power !== 0.0) {
-            setCurrent(power / voltage);
-            setResistance(voltage / current);
+            const i = Calculator.calculateCurrent(power, voltage, undefined);
+            const r = Calculator.calculateResistance(power, voltage, undefined);
+
+            if (i && r) {
+                setCurrent(i);
+                setResistance(r);
+            }
+            else throw Error(CALCULATE_FAIL_MESSAGE);
         }
     };
 
@@ -98,118 +136,126 @@ const ConvertElectrical: React.FC = () => {
                             <Row>
                                 <Tab.Content as={Col}>
                                     <Tab.Pane eventKey="calculator" title="Calculator">
-                                        <InputGroup>
-                                            <InputGroup.Prepend>
-                                                <InputGroup.Text>Resistance (<b>Ω</b>)</InputGroup.Text>
-                                            </InputGroup.Prepend>
-                                            <Form.Control
-                                                id="resistance"
-                                                type="number"
-                                                min={0.0}
-                                                max={MAXIMUM_RESISTANCE}
-                                                step="any"
-                                                value={resistance}
-                                                onChange={(e) => {
-                                                    const value = parseFloat(e.target.value);
-                                                    setResistance(value);
-                                                    update(0, value > 0.0);
-                                                }}
-                                            />
-                                        </InputGroup>
-                                        <InputGroup>
-                                            <InputGroup.Prepend>
-                                                <InputGroup.Text>Current (<b>A</b>)</InputGroup.Text>
-                                            </InputGroup.Prepend>
-                                            <Form.Control
-                                                id="current"
-                                                type="number"
-                                                min={0.0}
-                                                max={MAXIMUM_CURRENT}
-                                                step="any"
-                                                value={current}
-                                                onChange={(e) => {
-                                                    const value = parseFloat(e.target.value);
-                                                    setCurrent(value);
-                                                    update(1, value > 0.0);
-                                                }}
-                                            />
-                                        </InputGroup>
-                                        <InputGroup>
-                                            <InputGroup.Prepend>
-                                                <InputGroup.Text>Voltage (<b>V</b>)</InputGroup.Text>
-                                            </InputGroup.Prepend>
-                                            <Form.Control
-                                                id="voltage"
-                                                type="number"
-                                                min={0.0}
-                                                max={MAXIMUM_VOLTAGE}
-                                                step="any"
-                                                value={voltage}
-                                                onChange={(e) => {
-                                                    const value = parseFloat(e.target.value);
-                                                    setVoltage(value);
-                                                    update(2, value > 0.0);
-                                                }}
-                                            />
-                                        </InputGroup>
-                                        <InputGroup>
-                                            <InputGroup.Prepend>
-                                                <InputGroup.Text>Power (<b>W</b>)</InputGroup.Text>
-                                            </InputGroup.Prepend>
-                                            <Form.Control
-                                                id="power"
-                                                type="number"
-                                                min={0.0}
-                                                max={MAXIMUM_POWER}
-                                                step="any"
-                                                value={power}
-                                                onChange={(e) => {
-                                                    const value = parseFloat(e.target.value);
-                                                    setPower(value);
-                                                    update(3, value > 0.0);
-                                                }}
-                                            />
-                                        </InputGroup>
+                                        <Wrapper reverse>
+                                            <Row>
+                                                <InputGroup as={Col}>
+                                                    <InputGroup.Prepend>
+                                                        <InputGroup.Text>Resistance (<b>Ω</b>)</InputGroup.Text>
+                                                    </InputGroup.Prepend>
+                                                    <Form.Control
+                                                        id="resistance"
+                                                        type="number"
+                                                        min={0.0}
+                                                        max={MAXIMUM_RESISTANCE}
+                                                        step="any"
+                                                        value={resistance}
+                                                        onChange={(e) => {
+                                                            const value = parseFloat(e.target.value);
+                                                            setResistance(value);
+                                                            update(0, value > 0.0);
+                                                        }}
+                                                    />
+                                                </InputGroup>
+                                                <InputGroup as={Col}>
+                                                    <InputGroup.Prepend>
+                                                        <InputGroup.Text>Current (<b>A</b>)</InputGroup.Text>
+                                                    </InputGroup.Prepend>
+                                                    <Form.Control
+                                                        id="current"
+                                                        type="number"
+                                                        min={0.0}
+                                                        max={MAXIMUM_CURRENT}
+                                                        step="any"
+                                                        value={current}
+                                                        onChange={(e) => {
+                                                            const value = parseFloat(e.target.value);
+                                                            setCurrent(value);
+                                                            update(1, value > 0.0);
+                                                        }}
+                                                    />
+                                                </InputGroup>
+                                                <InputGroup as={Col}>
+                                                    <InputGroup.Prepend>
+                                                        <InputGroup.Text>Voltage (<b>V</b>)</InputGroup.Text>
+                                                    </InputGroup.Prepend>
+                                                    <Form.Control
+                                                        id="voltage"
+                                                        type="number"
+                                                        min={0.0}
+                                                        max={MAXIMUM_VOLTAGE}
+                                                        step="any"
+                                                        value={voltage}
+                                                        onChange={(e) => {
+                                                            const value = parseFloat(e.target.value);
+                                                            setVoltage(value);
+                                                            update(2, value > 0.0);
+                                                        }}
+                                                    />
+                                                </InputGroup>
+                                                <InputGroup as={Col}>
+                                                    <InputGroup.Prepend>
+                                                        <InputGroup.Text>Power (<b>W</b>)</InputGroup.Text>
+                                                    </InputGroup.Prepend>
+                                                    <Form.Control
+                                                        id="power"
+                                                        type="number"
+                                                        min={0.0}
+                                                        max={MAXIMUM_POWER}
+                                                        step="any"
+                                                        value={power}
+                                                        onChange={(e) => {
+                                                            const value = parseFloat(e.target.value);
+                                                            setPower(value);
+                                                            update(3, value > 0.0);
+                                                        }}
+                                                    />
+                                                </InputGroup>
+                                            </Row>
+                                        </Wrapper>
                                     </Tab.Pane>
                                     <Tab.Pane eventKey="information" title="Information">
                                         <MathJax.Provider>
-                                            <h1>Ohms</h1>
+                                            <h1 className="p-3 border-bottom">Ohms</h1>
+                                            <br />
                                             <div className="p-3 border">
-                                                <MathJax.Node  formula={'R = V / I'} />
+                                                <MathJax.Node formula={'R = \\frac{V}{I}'} />
                                                 <br/>
-                                                <MathJax.Node  formula={'R = V^2 / P'} />
+                                                <MathJax.Node formula={'R = \\frac{V^2}{P}'} />
                                                 <br/>
-                                                <MathJax.Node  formula={'R = P / I^2'} />
+                                                <MathJax.Node formula={'R = \\frac{P}{I^2}'} />
                                             </div>
                                         </MathJax.Provider>
                                         <MathJax.Provider>
-                                            <h1>Amps</h1>
+                                            <h1 className="p-3 border-bottom">Amps</h1>
+                                            <br />
                                             <div className="p-3 border">
-                                                <MathJax.Node  formula={'I = V / R'} />
+                                                <MathJax.Node formula={'I = \\frac{V}{R}'} />
                                                 <br/>
-                                                <MathJax.Node  formula={'I = P / V'} />
+                                                <MathJax.Node formula={'I = \\frac{P}{V}'} />
                                                 <br/>
-                                                <MathJax.Node  formula={'I = sqrt(P / R)'} />
+                                                <MathJax.Node formula={'I = \\sqrt{\\frac{P}{R}}'} />
                                             </div>
                                         </MathJax.Provider>
                                         <MathJax.Provider>
-                                            <h1>Volts</h1>
+                                            <h1 className="p-3 border-bottom">Volts</h1>
+                                            <br />
                                             <div className="p-3 border">
-                                                <MathJax.Node  formula={'V = I * R'} />
+                                                <MathJax.Node formula={'V = I\\cdot{R}'} />
                                                 <br/>
-                                                <MathJax.Node  formula={'V = P / I'} />
+                                                <MathJax.Node formula={'V = \\frac{P}{I}'} />
                                                 <br/>
-                                                <MathJax.Node  formula={'V = sqrt(P * R)'} />
+                                                <MathJax.Node formula={'V = \\sqrt{P\\cdot{R}}'} />
                                             </div>
                                         </MathJax.Provider>
                                         <MathJax.Provider>
-                                            <h1>Watts</h1>
+                                            <h1 className="p-3 border-bottom">Watts</h1>
+                                            <br />
                                             <div className="p-3 border">
-                                                <MathJax.Node  formula={'P = V * I'} />
+                                                <MathJax.Node formula={'P = V\\cdot{I}'} />
                                                 <br/>
-                                                <MathJax.Node  formula={'P = V^2 / R'} />
+                                                <MathJax.Node formula={'P = \\frac{V^2}{R}'} />
                                                 <br/>
-                                                <MathJax.Node  formula={'P = I^2 * R'} />
+                                                <MathJax.Node formula={'P = I^2\\cdot{R}'} />
                                             </div>
                                         </MathJax.Provider>
                                     </Tab.Pane>
